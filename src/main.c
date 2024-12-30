414bb714665de4e2be038487c50cc57ee41d2ee1
Marc Singleton <marcsingleton@berkeley.edu>
2024-12-30 15:12:22 -0600

    Added broad outline of program to main.c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,23 @@
 
 int main(int argc, char *argv[])
 {
+    // Process arguments
+
+    // Set screen and terminal options
+
+    // Read files
+    // - Try to match extensions
+    // - If no matching extension, infer with sniffer
+    // - Error if no sniffer is successful
+    // - Error if an explicit or inferred format is malformed
+
+    // Main loop
+    // Display current file
+    // Read input
+    // Process input
+
+    // Clean up
+    // Free memory and restore terminal options
     printf("Hello world!");
     return 0;
 }
