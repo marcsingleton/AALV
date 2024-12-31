7790112a72e113a1e1a6f64a9f0d4ead6b2d83e2
Marc Singleton <marcsingleton@berkeley.edu>
2024-12-30 19:48:25 -0500

    Added simple versions of outlines

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     // Process arguments
+    if (argc != 2)
+    {
+        fputs("Incorrect number of arguments. Quitting...\n", stderr);
+        return 1;
+    }
 
     // Set screen and terminal options
 
@@ -11,14 +17,28 @@ int main(int argc, char *argv[])
     // - If no matching extension, infer with sniffer
     // - Error if no sniffer is successful
     // - Error if an explicit or inferred format is malformed
+    FILE *alignment = fopen(argv[1], "r");
+    if (alignment == NULL)
+    {
+        fputs("Failed to open file. Quitting...\n", stderr);
+        return 1;
+    }
 
     // Main loop
     // Display current file
     // Read input
     // Process input
+    char *line = NULL;
+    size_t capacity = 0;
+    ssize_t linelen;
+    while ((linelen = getline(&line, &capacity, alignment)) > 0)
+    {
+        fputs(line, stdout);
+    }
 
     // Clean up
     // Free memory and restore terminal options
-    printf("Hello world!");
+    free(line);
+    fclose(alignment);
     return 0;
 }
