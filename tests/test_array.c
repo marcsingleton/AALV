d391777e931181aadd79e562ff4ec4e32857e659
Marc Singleton <marcsingleton@berkeley.edu>
2025-02-23 11:26:32 -0800

    Added test_append

--- tests/test_array.c
+++ tests/test_array.c
@@ -1,11 +1,27 @@
+#include <assert.h>
 #include <stdio.h>
 
 #include "array.h"
 
-void test_append()
+void test_init_capacity()
 {
     Array *array = create_array(1);
-    printf("Ran test array");
+}
+
+void test_append()
+{
+    Array *array = create_array(sizeof(1));
+    assert(array->capacity == 16);
+    printf("Assertion successful!\n");
+    for (int i = 0; i < 17; i++)
+    {
+        append_array(&i, array);
+    }
+    for (int i = 0; i < 17; i++)
+    {
+        int x = *(int *)get_array(i, array);
+        printf("Got element %d\n", x);
+    }
 }
 
 int main()
