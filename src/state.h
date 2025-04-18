6b353a4b535917543c487741743e7d28bda89ce4
Marc Singleton <marcsingleton@berkeley.edu>
2025-04-18 12:08:14 -0700

    Added members to State to track display variables

--- src/state.h
+++ src/state.h
@@ -14,6 +14,13 @@ typedef struct
     struct termios old_termios;
     struct termios raw_termios;
     SeqRecordArray record_array;
+    unsigned int terminal_rows;
+    unsigned int terminal_cols;
+    unsigned int cursor_i;
+    unsigned int cursor_j;
+    unsigned int header_pane_width;
+    unsigned int ruler_pane_height;
+    size_t record_offset;
 } State;
 
 #endif // STATE_H
