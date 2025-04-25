4cc0f858ecb472ae94e38ba4ce038287b70c8d4c
Marc Singleton <marcsingleton@berkeley.edu>
2025-04-24 23:10:01 -0700

    Added several display control actions

--- src/input.c
+++ src/input.c
@@ -97,6 +97,61 @@ int input_process_action(int action, Array *buffer)
         }
         break;
     }
+    case '$':
+    {
+        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+        SeqRecord record = state.record_array.records[record_index];
+        unsigned int sequence_pane_width = state.terminal_cols - state.header_pane_width;
+        if (record.len < sequence_pane_width)
+        {
+            state.offset_x = 0;
+            state.cursor_j = record.len + state.header_pane_width;
+        }
+        else
+        {
+            state.offset_x = record.len - sequence_pane_width;
+            state.cursor_j = state.terminal_cols;
+        }
+        display_ruler_pane(buffer);
+        display_ruler_pane_ticks(buffer);
+        display_sequence_pane(buffer);
+        break;
+    }
+    case '0':
+    case '^':
+    {
+        state.offset_x = 0;
+        state.cursor_j = state.header_pane_width + 1;
+        display_ruler_pane(buffer);
+        display_ruler_pane_ticks(buffer);
+        display_sequence_pane(buffer);
+        break;
+    }
+    case 'o':
+    {
+        unsigned int min_width = sizeof(HEADER_PANE_ELLIPSES) - 1 + 1; // Remove null, add separator
+        if (state.header_pane_width > min_width)
+        {
+            state.header_pane_width--;
+            display_ruler_pane(buffer);
+            display_ruler_pane_ticks(buffer);
+            display_header_pane(buffer);
+            display_sequence_pane(buffer);
+        }
+        break;
+    }
+    case 'p':
+    {
+        if (state.header_pane_width < state.terminal_cols - 1)
+        {
+            state.header_pane_width++;
+            display_ruler_pane(buffer);
+            display_ruler_pane_ticks(buffer);
+            display_header_pane(buffer);
+            display_sequence_pane(buffer);
+        }
+        break;
+    }
     case 'q':
         exit(0);
         break;
