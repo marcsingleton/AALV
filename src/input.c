0da7903163af901f0cd08a7c4c53f9a81f5d150e
Marc Singleton <marcsingleton@berkeley.edu>
2025-04-30 00:00:20 -0400

    Cleaned up input.c by making cases into functions

--- src/input.c
+++ src/input.c
@@ -27,121 +27,30 @@ int input_process_action(int action, Array *buffer)
     switch (action)
     {
     case 'j':
-    {
-        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
-        if (record_index < state.record_array.len - 1)
-        {
-            if (state.cursor_i < state.terminal_rows)
-                state.cursor_i++;
-            else
-            {
-                state_set_offset_y(&state, state.offset_y + 1);
-            }
-        }
+        input_move_down();
         break;
-    }
     case 'k':
-    {
-        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
-        if (record_index > 0)
-        {
-            if (state.cursor_i > state.ruler_pane_height + 1)
-                state.cursor_i--;
-            else
-            {
-                state_set_offset_y(&state, state.offset_y - 1);
-            }
-        }
+        input_move_up();
         break;
-    }
-    case 'h':
-    {
-        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
-        SeqRecord record = state.record_array.records[record_index];
-        unsigned int cursor_x = state.cursor_j + state.offset_x - state.header_pane_width;
-        if (cursor_x > record.len)
-        {
-            if (record.len < state.offset_x + 1)
-            {
-                state_set_offset_x(&state, record.len - 1);
-                state.cursor_j = state.header_pane_width + 1;
-            }
-            else if (record.len == state.offset_x + 1)
-            {
-                state_set_offset_x(&state, state.offset_x - 1);
-                state.cursor_j = state.header_pane_width + 1;
-            }
-            else
-            {
-                state.cursor_j = record.len - state.offset_x + state.header_pane_width - 1;
-            }
-        }
-        else if (state.cursor_j > state.header_pane_width + 1)
-        {
-            state.cursor_j--;
-        }
-        else if (state.cursor_j == state.header_pane_width + 1 && state.offset_x > 0)
-        {
-            state_set_offset_x(&state, state.offset_x - 1);
-        }
-        break;
-    }
     case 'l':
-    {
-        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
-        SeqRecord record = state.record_array.records[record_index];
-        unsigned int cursor_x = state.cursor_j + state.offset_x - state.header_pane_width;
-        if (state.cursor_j < state.terminal_cols && cursor_x < record.len)
-        {
-            state.cursor_j++;
-        }
-        else if (state.cursor_j == state.terminal_cols && cursor_x < record.len)
-        {
-            state_set_offset_x(&state, state.offset_x + 1);
-        }
+        input_move_right();
         break;
-    }
+    case 'h':
+        input_move_left();
+        break;
     case '$':
-    {
-        unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
-        SeqRecord record = state.record_array.records[record_index];
-        unsigned int sequence_pane_width = state.terminal_cols - state.header_pane_width;
-        if (record.len < sequence_pane_width)
-        {
-            state_set_offset_x(&state, 0);
-            state.cursor_j = record.len + state.header_pane_width;
-        }
-        else
-        {
-            state_set_offset_x(&state, record.len - sequence_pane_width);
-            state.cursor_j = state.terminal_cols;
-        }
+        input_move_line_end();
         break;
-    }
     case '0':
     case '^':
-    {
-        state_set_offset_x(&state, 0);
-        state.cursor_j = state.header_pane_width + 1;
+        input_move_line_start();
         break;
-    }
-    case 'o':
-    {
-        unsigned int min_width = sizeof(HEADER_PANE_ELLIPSES) - 1 + 1; // Remove null, add separator
-        if (state.header_pane_width > min_width)
-        {
-            state_set_header_pane_width(&state, state.header_pane_width - 1);
-        }
+    case ']':
+        input_increase_header_pane_width();
         break;
-    }
-    case 'p':
-    {
-        if (state.header_pane_width < state.terminal_cols - 1)
-        {
-            state_set_header_pane_width(&state, state.header_pane_width + 1);
-        }
+    case '[':
+        input_decrease_header_pane_width();
         break;
-    }
     case 'q':
         exit(0);
         break;
@@ -175,4 +84,101 @@ void input_buffer_flush(Array *buffer)
 {
     write(STDOUT_FILENO, buffer->data, buffer->len);
     buffer->len = 0;
+}
+
+void input_move_up(void)
+{
+    unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+    if (record_index > 0)
+    {
+        if (state.cursor_i > state.ruler_pane_height + 1)
+            state.cursor_i--;
+        else
+            state_set_offset_y(&state, state.offset_y - 1);
+    }
+}
+
+void input_move_down(void)
+{
+    unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+    if (record_index < state.record_array.len - 1)
+    {
+        if (state.cursor_i < state.terminal_rows)
+            state.cursor_i++;
+        else
+            state_set_offset_y(&state, state.offset_y + 1);
+    }
+}
+
+void input_move_right(void)
+{
+    unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+    SeqRecord record = state.record_array.records[record_index];
+    unsigned int cursor_x = state.cursor_j + state.offset_x - state.header_pane_width;
+    if (state.cursor_j < state.terminal_cols && cursor_x < record.len)
+        state.cursor_j++;
+    else if (state.cursor_j == state.terminal_cols && cursor_x < record.len)
+        state_set_offset_x(&state, state.offset_x + 1);
+}
+
+void input_move_left(void)
+{
+    unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+    SeqRecord record = state.record_array.records[record_index];
+    unsigned int cursor_x = state.cursor_j + state.offset_x - state.header_pane_width;
+    if (cursor_x > record.len)
+    {
+        if (record.len < state.offset_x + 1)
+        {
+            state_set_offset_x(&state, record.len - 1);
+            state.cursor_j = state.header_pane_width + 1;
+        }
+        else if (record.len == state.offset_x + 1)
+        {
+            state_set_offset_x(&state, state.offset_x - 1);
+            state.cursor_j = state.header_pane_width + 1;
+        }
+        else
+            state.cursor_j = record.len - state.offset_x + state.header_pane_width - 1;
+    }
+    else if (state.cursor_j > state.header_pane_width + 1)
+        state.cursor_j--;
+    else if (state.cursor_j == state.header_pane_width + 1 && state.offset_x > 0)
+        state_set_offset_x(&state, state.offset_x - 1);
+}
+
+void input_move_line_start(void)
+{
+    state_set_offset_x(&state, 0);
+    state.cursor_j = state.header_pane_width + 1;
+}
+
+void input_move_line_end(void)
+{
+    unsigned int record_index = state.cursor_i + state.offset_y - state.ruler_pane_height - 1;
+    SeqRecord record = state.record_array.records[record_index];
+    unsigned int sequence_pane_width = state.terminal_cols - state.header_pane_width;
+    if (record.len < sequence_pane_width)
+    {
+        state_set_offset_x(&state, 0);
+        state.cursor_j = record.len + state.header_pane_width;
+    }
+    else
+    {
+        state_set_offset_x(&state, record.len - sequence_pane_width);
+        state.cursor_j = state.terminal_cols;
+    }
+}
+
+void input_increase_header_pane_width(void)
+{
+    if (state.header_pane_width < state.terminal_cols - 1)
+        state_set_header_pane_width(&state, state.header_pane_width + 1);
+}
+
+void input_decrease_header_pane_width(void)
+{
+    unsigned int min_width = sizeof(HEADER_PANE_ELLIPSES) - 1 + 1; // Remove null, add separator
+    if (state.header_pane_width > min_width)
+        state_set_header_pane_width(&state, state.header_pane_width - 1);
 }
--- src/input.h
+++ src/input.h
@@ -10,5 +10,13 @@
 int input_get_action(void);
 int input_process_action(int action, Array *buffer);
 void input_buffer_flush(Array *buffer);
+void input_move_up(void);
+void input_move_down(void);
+void input_move_right(void);
+void input_move_left(void);
+void input_move_line_start(void);
+void input_move_line_end(void);
+void input_increase_header_pane_width(void);
+void input_decrease_header_pane_width(void);
 
 #endif // INPUT_H
