46e3744e2c5e3dd97fc0769210a532c6c58fe2f5
Marc Singleton <marcsingleton@berkeley.edu>
2025-05-05 22:24:49 -0400

    Refactored display functions to loop relative to pane coordinates

--- src/display.c
+++ src/display.c
@@ -18,39 +18,39 @@ void display_all_panes(Array *buffer)
 
 void display_header_pane(Array *buffer)
 {
-    for (unsigned int i = state.ruler_pane_height + 1; i <= state.terminal_rows - 2; i++)
-    {
-        unsigned int record_index = i + state.offset_record - state.ruler_pane_height - 1;
+    unsigned int record_panes_height = state_get_record_panes_height(&state);
 
-        terminal_cursor_ij(buffer, i, 1);
+    terminal_cursor_ij(buffer, state.ruler_pane_height + 1, state.header_pane_width);
+    for (unsigned int i = 0; i < record_panes_height; i++)
+    {
+        terminal_clear_line_left(buffer);
+        char s[] = "┃\n\b";
+        array_extend(buffer, s, sizeof(s) - 1);
+    }
+
+    for (unsigned int i = 0; i < record_panes_height; i++)
+    {
+        unsigned int record_index = i + state.offset_record;
+
+        terminal_cursor_ij(buffer, i + state.ruler_pane_height + 1, 1);
         if (record_index < state.record_array.len)
         {
             SeqRecord record = state.record_array.records[record_index];
             unsigned int len = strnlen(record.header, state.header_pane_width);
-            if (len <= state.header_pane_width - 1)
-            {
+            if (len < state.header_pane_width)
                 array_extend(buffer, record.header, len);
-                for (unsigned int j = len + 1; j <= state.header_pane_width - 1; j++)
-                    array_append(buffer, " ");
-            }
             else
             {
                 array_extend(buffer, record.header, state.header_pane_width - 4);
                 array_extend(buffer, HEADER_PANE_ELLIPSES, sizeof(HEADER_PANE_ELLIPSES) - 1);
             }
-            array_extend(buffer, "┃", sizeof("┃") - 1);
         }
         else
-        {
             array_extend(buffer, "~", sizeof("~") - 1);
-            for (unsigned int j = 2; j <= state.header_pane_width - 1; j++)
-                array_append(buffer, " ");
-            array_extend(buffer, "┃", sizeof("┃") - 1);
-        }
     }
 
-    terminal_cursor_ij(buffer, state.terminal_rows - 1, 1);
-    for (unsigned int j = 1; j <= state.header_pane_width - 1; j++)
+    terminal_cursor_ij(buffer, state.ruler_pane_height + record_panes_height + 1, 1);
+    for (unsigned int j = 0; j < state.header_pane_width - 1; j++)
     {
         array_extend(buffer, "─", sizeof("─") - 1);
     }
@@ -60,18 +60,19 @@ void display_header_pane(Array *buffer)
 void display_ruler_pane(Array *buffer)
 {
     terminal_cursor_ij(buffer, 1, state.header_pane_width);
-    for (unsigned int i = 1; i <= state.ruler_pane_height - 1; i++)
+    for (unsigned int i = 0; i < state.ruler_pane_height; i++)
     {
         terminal_clear_line(buffer);
         char s[] = "│\n\b";
         array_extend(buffer, s, sizeof(s) - 1);
     }
 
+    unsigned int sequence_pane_width = state_get_sequence_pane_width(&state);
     terminal_cursor_ij(buffer, state.ruler_pane_height, 1);
-    for (unsigned int j = 1; j <= state.header_pane_width - 1; j++)
+    for (unsigned int j = 0; j < state.header_pane_width - 1; j++)
         array_extend(buffer, "─", sizeof("─") - 1);
     array_extend(buffer, "╆", sizeof("╆") - 1);
-    for (unsigned int j = state.header_pane_width + 1; j <= state.terminal_cols; j++)
+    for (unsigned int j = 0; j < sequence_pane_width; j++)
         array_extend(buffer, "━", sizeof("━") - 1);
 }
 
@@ -85,9 +86,10 @@ void display_ruler_pane_ticks(Array *buffer)
 
     unsigned int x = q * tick_spacing;
     unsigned int j;
-    while ((j = x - state.offset_sequence + state.header_pane_width) <= state.terminal_cols)
+    unsigned int sequence_pane_width = state_get_sequence_pane_width(&state);
+    while ((j = x - state.offset_sequence) < sequence_pane_width)
     {
-        terminal_cursor_ij(buffer, state.ruler_pane_height, j);
+        terminal_cursor_ij(buffer, state.ruler_pane_height, j + state.header_pane_width + 1);
         array_extend(buffer, "┷", sizeof("┷") - 1);
 
         char c[2];
@@ -96,10 +98,10 @@ void display_ruler_pane_ticks(Array *buffer)
         unsigned int i = state.ruler_pane_height - 1;
         do
         {
-            d = n % 10;
-            n = n / 10;
+            d = n % tick_spacing;
+            n = n / tick_spacing;
             snprintf(c, 2, "%d", d);
-            terminal_cursor_ij(buffer, i--, j);
+            terminal_cursor_ij(buffer, i--, j + state.header_pane_width + 1);
             array_append(buffer, c);
         } while (n != 0);
 
@@ -109,11 +111,13 @@ void display_ruler_pane_ticks(Array *buffer)
 
 void display_sequence_pane(Array *buffer)
 {
-    for (unsigned int i = state.ruler_pane_height + 1; i <= state.terminal_rows - 2; i++)
+    unsigned int record_panes_height = state_get_record_panes_height(&state);
+    unsigned int sequence_pane_width = state_get_sequence_pane_width(&state);
+    for (unsigned int i = 0; i < record_panes_height; i++)
     {
-        unsigned int record_index = i + state.offset_record - state.ruler_pane_height - 1;
+        unsigned int record_index = i + state.offset_record;
 
-        terminal_cursor_ij(buffer, i, state.header_pane_width + 1);
+        terminal_cursor_ij(buffer, i + state.ruler_pane_height + 1, state.header_pane_width + 1);
         terminal_clear_line_right(buffer);
         if (record_index < state.record_array.len)
         {
@@ -132,11 +136,13 @@ void display_sequence_pane(Array *buffer)
             else
                 array_extend(buffer, record.seq + state.offset_sequence + 1, len - 1);
         }
-
-        terminal_cursor_ij(buffer, state.terminal_rows - 1, state.header_pane_width + 1);
-        for (unsigned int j = state.header_pane_width + 1; j <= state.terminal_cols; j++)
-            array_extend(buffer, "━", sizeof("━") - 1);
     }
+
+    terminal_cursor_ij(buffer,
+                       state.ruler_pane_height + record_panes_height + 1,
+                       state.header_pane_width + 1);
+    for (unsigned int j = 0; j < sequence_pane_width; j++)
+        array_extend(buffer, "━", sizeof("━") - 1);
 }
 
 void display_cursor(Array *buffer)
