026128cdd24326b137e97a8ef47d5701e0675329
Marc Singleton <marcsingleton@berkeley.edu>
2025-06-08 12:41:01 -0700

    Separated state into global and file state

--- src/state.c
+++ src/state.c
@@ -4,16 +4,18 @@
 #include "display.h"
 #include "state.h"
 
+// FileState setters
 void state_set_header_pane_width(State *state, unsigned int header_pane_width)
 {
+    FileState *active_file = state->active_file;
     unsigned int min_width = wcswidth(HEADER_PANE_ELLIPSES, sizeof(HEADER_PANE_ELLIPSES)) + 1;
     if (header_pane_width > state->terminal_cols - 2)
         header_pane_width = state->terminal_cols - 2;
     if (header_pane_width < min_width)
         header_pane_width = min_width;
-    if (header_pane_width != state->header_pane_width)
+    if (header_pane_width != active_file->header_pane_width)
     {
-        state->header_pane_width = header_pane_width;
+        active_file->header_pane_width = header_pane_width;
         state->refresh_ruler_pane = true;
         state->refresh_header_pane = true;
         state->refresh_sequence_pane = true;
@@ -23,15 +25,15 @@ void state_set_header_pane_width(State *state, unsigned int header_pane_width)
 
 void state_set_ruler_pane_height(State *state, unsigned int ruler_pane_height)
 {
-
+    FileState *active_file = state->active_file;
     unsigned int min_height = wcswidth(RULER_PANE_ELLIPSES, sizeof(RULER_PANE_ELLIPSES)) + 1;
     if (ruler_pane_height > state->terminal_rows - 2)
         ruler_pane_height = state->terminal_rows - 2;
     if (ruler_pane_height < min_height)
         ruler_pane_height = min_height;
-    if (ruler_pane_height != state->ruler_pane_height)
+    if (ruler_pane_height != active_file->ruler_pane_height)
     {
-        state->ruler_pane_height = ruler_pane_height;
+        active_file->ruler_pane_height = ruler_pane_height;
         state->refresh_ruler_pane = true;
         state->refresh_header_pane = true;
         state->refresh_sequence_pane = true;
@@ -40,41 +42,65 @@ void state_set_ruler_pane_height(State *state, unsigned int ruler_pane_height)
 
 void state_set_tick_spacing(State *state, unsigned int tick_spacing)
 {
+    FileState *active_file = state->active_file;
     if (tick_spacing < 1)
         tick_spacing = 1;
-    if (tick_spacing != state->tick_spacing)
+    if (tick_spacing != active_file->tick_spacing)
     {
-        state->tick_spacing = tick_spacing;
+        active_file->tick_spacing = tick_spacing;
         state->refresh_ruler_pane = true;
     }
 }
 
 void state_set_offset_record(State *state, unsigned int offset_record)
 {
-    state->offset_record = offset_record;
+    FileState *active_file = state->active_file;
+    active_file->offset_record = offset_record;
     state->refresh_header_pane = true;
     state->refresh_sequence_pane = true;
 }
 
 void state_set_offset_sequence(State *state, unsigned int offset_sequence)
 {
-    state->offset_sequence = offset_sequence;
+    FileState *active_file = state->active_file;
+    active_file->offset_sequence = offset_sequence;
     state->refresh_ruler_pane = true;
     state->refresh_sequence_pane = true;
 }
 
+inline void state_set_cursor_record_i(State *state, unsigned int cursor_record_i)
+{
+    FileState *active_file = state->active_file;
+    active_file->cursor_record_i = cursor_record_i;
+}
+
+inline void state_set_cursor_header_j(State *state, unsigned int cursor_header_j)
+{
+    FileState *active_file = state->active_file;
+    active_file->cursor_header_j = cursor_header_j;
+}
+
+inline void state_set_cursor_sequence_j(State *state, unsigned int cursor_sequence_j)
+{
+    FileState *active_file = state->active_file;
+    active_file->cursor_sequence_j = cursor_sequence_j;
+}
+
+// FileState getters
 unsigned int state_get_record_panes_height(State *state)
 {
-    if (state->terminal_rows <= state->ruler_pane_height + 2)
+    FileState *active_file = state->active_file;
+    if (state->terminal_rows <= active_file->ruler_pane_height + 2)
         return 0;
     else
-        return state->terminal_rows - state->ruler_pane_height - 2;
+        return state->terminal_rows - active_file->ruler_pane_height - 2;
 }
 
 unsigned int state_get_sequence_pane_width(State *state)
 {
-    if (state->terminal_cols <= state->header_pane_width)
+    FileState *active_file = state->active_file;
+    if (state->terminal_cols <= active_file->header_pane_width)
         return 0;
     else
-        return state->terminal_cols - state->header_pane_width;
+        return state->terminal_cols - active_file->header_pane_width;
 }
--- src/state.h
+++ src/state.h
@@ -8,15 +8,11 @@
 #include <stdbool.h>
 #include <termios.h>
 
+#include "array.h"
 #include "sequences.h"
 
 typedef struct
 {
-    struct termios old_termios;
-    struct termios raw_termios;
-    SeqRecordArray record_array;
-    unsigned int terminal_rows;
-    unsigned int terminal_cols;
     unsigned int header_pane_width;
     unsigned int ruler_pane_height;
     unsigned int tick_spacing;
@@ -26,19 +22,40 @@ typedef struct
     unsigned int cursor_record_i;   // Row index in header/sequence panes
     unsigned int cursor_header_j;   // Column index in header pane
     unsigned int cursor_sequence_j; // Column index in sequence pane
+    SeqRecordArray record_array;
+} FileState;
+
+typedef struct
+{
+    // Global state variables
+    struct termios old_termios;
+    struct termios raw_termios;
+    unsigned int terminal_rows;
+    unsigned int terminal_cols;
     bool refresh_ruler_pane;
     bool refresh_header_pane;
     bool refresh_sequence_pane;
     bool refresh_command_pane;
     bool refresh_window;
+    // File state variables
+    FileState *files;
+    unsigned int nfiles;
+    FileState *active_file;
+    unsigned int active_file_index;
 } State;
 
+// FileState setters
 void state_set_header_pane_width(State *state, unsigned int header_pane_width);
 void state_set_ruler_pane_height(State *state, unsigned int ruler_pane_height);
 void state_set_tick_spacing(State *state, unsigned int tick_spacing);
 void state_set_offset_record(State *state, unsigned int offset_record);
 void state_set_offset_header(State *state, unsigned int offset_header);
 void state_set_offset_sequence(State *state, unsigned int offset_sequence);
+inline void state_set_cursor_record_i(State *state, unsigned int cursor_record_i);
+inline void state_set_cursor_header_j(State *state, unsigned int cursor_header_j);
+inline void state_set_cursor_sequence_j(State *state, unsigned int cursor_sequence_j);
+
+// FileState getters
 unsigned int state_get_record_panes_height(State *state);
 unsigned int state_get_sequence_pane_width(State *state);
 
