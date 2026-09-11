3c99cdfb9b713228620001bfffe28862ae027024
Marc Singleton <marcsingleton@berkeley.edu>
2026-09-10 21:36:11 -0700

    Changed formatting of test output to allow multiple lines

--- tests/utils.h
+++ tests/utils.h
@@ -5,7 +5,12 @@
  * Test runners
  */
 
+#include <stdarg.h>
 #include <stdio.h>
+
+#define TEST_SUCCESS 0
+#define TEST_FAILURE -1
+#define TEST_RESULT_PREFIX "    "
 
 typedef struct
 {
@@ -13,21 +18,30 @@ typedef struct
     char *name;
 } Test;
 
+void test_result_printf(const char *format, ...)
+{
+    va_list ap;
+    fputs(TEST_RESULT_PREFIX, stdout);
+    va_start(ap, format);
+    printf(format, ap);
+    va_end(ap);
+}
+
 void run_tests(Test *tests, int ntests, char *module_name)
 {
     int npasses = 0;
     for (int i = 0; i < ntests; i++)
     {
         Test *test = tests + i;
-        printf("Running %s...", test->name);
+        printf("Running %s...\n", test->name);
         int retcode = (*test->fn_ptr)();
-        if (retcode == 0)
+        if (retcode == TEST_SUCCESS)
         {
-            printf("passed!\n");
+            test_result_printf("\x1b[32mPassed!\x1b[39m\n");
             npasses++;
         }
         else
-            printf("failed with code %d.\n", retcode);
+            test_result_printf("\x1b[31mFailed.\x1b[39m\n");
     }
     printf("%d / %d tests passed in module %s\n", npasses, ntests, module_name);
 }
